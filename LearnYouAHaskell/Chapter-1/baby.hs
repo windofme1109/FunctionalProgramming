66d63de1e70d09bfca03a611c884d0044e78dd35
qinmr <qinmr@yonyou.com>
2020-11-04 12:33:21 +0800

    添加注释说明

--- LearnYouAHaskell/Chapter-1/baby.hs
+++ LearnYouAHaskell/Chapter-1/baby.hs
@@ -1,6 +1,9 @@
+{-
+    多行注释
+-}
 doubleMe x = x + x
 doubleUs x y = x * 2 + y * 2
-
+-- 定义一个函数 （单行注释）
 doubleSmallNumber x = if x > 100
                         then x
                         else x * 2
